38476b9a84132af6b93f7c9c73ff34c3dc17042f
executionreverted <canersevince3@gmail.com>
2023-12-11 15:53:36 +0300

    adventure contract

--- contracts/DiamondApp/facets/AdventureFacet.sol
+++ contracts/DiamondApp/facets/AdventureFacet.sol
@@ -13,9 +13,13 @@ import {LibRandom} from "../libraries/LibRandom.sol";
 import {LibMeta} from "../../shared/libraries/LibMeta.sol";
 import {LibAdventure} from "../libraries/LibAdventure.sol";
 import {IDragonStonePieces} from "../erc20/IDragonStonePieces.sol";
+import {LibRewards} from "../libraries/LibRewards.sol";
+import {LibLevel} from "../libraries/LibLevel.sol";
 
 contract AdventureFacet is Modifiers {
-    function enterAdventure(uint adventureId) external onlyRegistered {
+    function enterAdventure(
+        uint adventureId
+    ) external onlyNonEOA onlyRegistered {
         address player = LibMeta.msgSender();
         Player memory state = s.PlayerState[player];
         AdventureMap memory map = LibAdventure.getMap(adventureId);
@@ -27,16 +31,75 @@ contract AdventureFacet is Modifiers {
         );
 
         // enter adventure state
-        s.PlayerState[player].ACTION_STATE = PlayerAction.ADVENTURE;
-        s.PlayerState[player].ACTION_START = block.timestamp;
-        s.PlayerState[player].ACTION_DATA1 = adventureId;
+        _enterAdventure(player, adventureId);
+    }
+
+    function reenterAdventure() external onlyNonEOA onlyRegistered {
+        address player = LibMeta.msgSender();
+        require(
+            s.PlayerState[player].ACTION_STATE == PlayerAction.ADVENTURE,
+            "AdventureFacet: not in adventure"
+        );
+        uint id = s.PlayerState[player].ACTION_DATA1;
+        require(id != 0, "AdventureFacet: invalid map");
+        AdventureMap memory map = LibAdventure.getMap(id);
+        finalizeAdventure(player, map);
+        _enterAdventure(player, id);
+    }
+
+    function leaveAdventure() external onlyNonEOA onlyRegistered {
+        address player = LibMeta.msgSender();
+        require(
+            s.PlayerState[player].ACTION_STATE == PlayerAction.ADVENTURE,
+            "AdventureFacet: not in adventure"
+        );
+        uint id = s.PlayerState[player].ACTION_DATA1;
+        require(id != 0, "AdventureFacet: invalid map");
+        AdventureMap memory map = LibAdventure.getMap(id);
+        finalizeAdventure(player, map);
     }
 
     function finalizeAdventure(
         address player,
         AdventureMap memory map
     ) internal {
-        
+        /* 
+        struct AdventureMap {
+            uint MIN_LEVEL;
+            uint BASE_CYCLE;
+            uint MAX_CYCLE;
+            uint BASE_DROP_AMOUNT;
+            uint BASE_GOLD_REWARD;
+            uint STONE_DROP_CHANCE;
+            uint STONE_DROP_MIN_TIME;
+            uint EXP_PER_CYCLE;
+            int[] MIN_STATS;
+        }
+        */
+        uint cycles = (block.timestamp - s.PlayerState[player].ACTION_START) /
+            map.BASE_CYCLE;
+        if (cycles > map.MAX_CYCLE) cycles = map.MAX_CYCLE;
+        LibRewards.mintPiece(player, cycles * map.BASE_DROP_AMOUNT);
+        LibRewards.mintGold(player, cycles * map.BASE_GOLD_REWARD);
+        LibLevel.giveExp(player, cycles * map.EXP_PER_CYCLE);
+        exitAdventure(player);
+    }
+
+    function _enterAdventure(address player, uint adventureId) internal {
+        s.PlayerState[player].ACTION_STATE = PlayerAction.ADVENTURE;
+        s.PlayerState[player].ACTION_START = block.timestamp;
+        s.PlayerState[player].ACTION_DATA1 = adventureId;
+    }
+
+    function exitAdventure(address player) internal {
+        s.PlayerState[player].ACTION_STATE = PlayerAction.FREE;
+        s.PlayerState[player].ACTION_START = block.timestamp;
+        s.PlayerState[player].ACTION_DATA1 = 0;
+    }
+
+    // BE CAREFUL IT WILL RESET YOUR ADVENTURE PROGRESS
+    function forceExit() external onlyNonEOA onlyRegistered {
+        exitAdventure(LibMeta.msgSender());
     }
 
     function getAllMaps() external pure returns (AdventureMap[] memory) {
